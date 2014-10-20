17a98a384b20447397971aa7c8598d272f5c1b46
Marquiste Boyce <mboyce84@gmail.com>
2014-10-19 22:45:28 -0400

    added nouns, verbs and initial classes

--- oop_blackjack.rb
+++ oop_blackjack.rb
@@ -1,17 +1,39 @@
-# Nouns - Player, Dealer, Deck, Card, Game
-# Verbs - Hit, Stay, Compare Hands, Replay Game
+# Title: Blackjack game built with Ruby (Object-Oriented)
+# Rules: he objective of the game is to beat the dealer. If your cards total higher than the dealer's cards without going over 21 you win. You are not trying to get # close to 21. If your hand or the dealer's hand goes over 21 you "Bust."
 
-class Player
+# Nouns: Player, Dealer, Deck, Card, Game
+# Verbs: Deal Cards, Hit, Stay, Compare Hands, Replay Game
+
+module Colorized_Ruby_Output
+  def red; "\033[31m#{self}\033[0m" end
+  def green; "\033[32m#{self}\033[0m" end
+  def magenta; "\033[35m#{self}\033[0m" end
+  def cyan; "\033[36m#{self}\033[0m" end
+  def bg_blue; "\033[44m#{self}\033[0m" end
+end
+
+class Player 
+    def initialize(name)
+      @name = name
+    end
 end
 
 class Dealer
 end
 
 class Deck
+  ...
 end
 
 class Card
+  ...
 end
 
 class Game
-end
+  include Colorized_Ruby_Output
+  def play
+  end
+ end
+
+
+game = Game.new.play
